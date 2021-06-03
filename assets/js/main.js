86dd003a4b9a4388286f860ad070b4aed5d949f8
Yorick van den Hurk <nowane@gmail.com>
2021-06-03 14:01:39 +0000

    Added objects to main.js

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+// card options
+const cardArray = [
+    {
+        name: 'fries',
+        image: 'assets/images/fries.png'
+    }
+    {
+        name: 'fries',
+        image: 'assets/images/fries.png'
+    }
+    {
+        name: 'cheeseburger',
+        image: 'assets/images/cheeseburger.png'
+    }
+    {
+        name: 'cheeseburger',
+        image: 'assets/images/cheeseburger.png'
+    }
+    {
+        name: 'hotdog',
+        image: 'assets/images/hotdog.png'
+    }
+    {
+        name: 'hotdog',
+        image: 'assets/images/hotdog.png'
+    }
+    {
+        name: 'ice-cream',
+        image: 'assets/images/ice-cream.png'
+    }
+    {
+        name: 'ice-cream',
+        image: 'assets/images/ice-cream.png'
+    }
+    {
+        name: 'milkshake',
+        image: 'assets/images/milkshake.png'
+    }
+    {
+        name: 'milkshake',
+        image: 'assets/images/milkshake.png'
+    }
+    {
+        name: 'pizza',
+        image: 'assets/images/pizza.png'
+    }
+    {
+        name: 'pizza',
+        image: 'assets/images/pizza.png'
+    }
+]
+
+const grid = document.querySelector.("grid");
+
+})
